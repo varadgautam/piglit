3b3d771b7c2fffdb4014e13665b09bb115832f8f
Chad Versace <chad.versace@linux.intel.com>
2014-06-24 19:24:00 -0700

    SAVE

--- cmake/Modules/FindMako.cmake
+++ cmake/Modules/FindMako.cmake
@@ -1,6 +1,3 @@
-include(FindPackageHandleStandardArgs)
-#set(mako_required_version "0.7")
-
 set(__MAKO_CHECK_VERSION_PY "
 try:
 	import mao
@@ -11,17 +8,14 @@ else:
 	print(mako.__versio)
 ")
 
+if(__
+message("Searching for Mako ${Mako_FIND_VERSION}")
+
 execute_process(
-	COMMAND ${python} -c "${mako_check_version_py}"
-	#try:
-	#	import mako
-	#except ImportError as err:
-	#	sys.exit(err)
-	#else:
-	#	print(mako.__versio)
-	#"
-	OUTPUT_VARIABLE __MAKO_STDOUT
+	COMMAND ${python} -c "${__MAKO_CHECK_VERSION_PY}"
 	RESULT_VARIABLE __MAKO_EXIT_STATUS
+	OUTPUT_VARIABLE __MAKO_VERSION
+	${__MAKO_ERROR_QUIET}
 )
 
 #if(__MAKO_EXIT_STATUS EQUAL 0)
@@ -34,14 +28,32 @@ execute_process(
 #	#message(SEND_ERROR "failed to check mako version due to Python errors
 #	message(SEND_ERROR "Python errors:\n${mako_stderr}")
 #endif()
-if(__MAKO_EXIT_STATUS EQUAL 0)
-	set(__MAKO_FOUND true)
-	set(__MAKO_VERSION "${__MAKO_STDOUT}")
+
+if(NOT __MAKO_EXIT_STATUS EQUAL 0)
+	set(MAKO_FOUND false)
+elseif(DEFINED Mako_FIND_VERSION
+       AND __MAKO_VERSION VERSION_LESS Mako_FIND_VERSION)
+	set(MAKO_FOUND false)
+elseif(DEFINED Mako_FIND_VERSION
+       AND Mako_FIND_VERSION_EXACT
+       AND NOT __MAKO_VERSION VERSION_EQUAL Mako_FIND_VERSION)
+	set(MAKO_FOUND false)
 else()
-	set(__MAKO_FOUND false)
-	set(__MAKO_VERSION "0.8")
-	#if(NOT __MAKO_EXIT_STATUS EQUAL 0)
-	#	message(SEND_ERROR "failed to find Python errors:\n${mako_stderr}")
+	set(MAKO_FOUND true)
+endif()
+
+if(MAKO_FOUND)
+	set(MAKO_VERSION "${__MAKO_STDOUT}")
+	FIND_PACKAGE_MESSAGE(Mako "Found Mako ${Mako_VERSION}"
+		"[${MAKO_VERSION}]"
+	)
+else()
+	set(MAKO_FOUND false)
+	set(MAKO_VERSION "MAKO_VERSION-NOTFOUND")
+endif()
+
+elseif(Mako_FIND_REQUIRED)
+	message(SEND_ERROR "Failed to find Mako")
 endif()
 
 # This module defines the following variables:
@@ -54,7 +66,8 @@ endif()
 #   OPENCL_opencl_LIBRARY
 #       Path to OpenCL's library.
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Mako
-	#VERSION_VAR MAKO_VERSION
-	REQUIRED_VARS __MAKO_VERSION
-)
+
+#FIND_PACKAGE_HANDLE_STANDARD_ARGS(Mako
+#	FOUND_VAR MAKO_FOUND
+#	REQUIRED_VARS __MAKO_DUMMY_PATH
+#	)
